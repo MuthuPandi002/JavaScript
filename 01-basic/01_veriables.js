7478684e8d336a1078f6db4616cb4ca562e29846
MuthuPandi002 <161207283+MuthuPandi002@users.noreply.github.com>
2024-04-29 12:59:00 +0000

    day 1

---
+++ 01-basic/01_veriables.js
@@ -0,0 +1,14 @@
+const account_id = 251463;
+var email_id  = "muthu@gmail.com";
+var password = 31548;
+City = "Mumbai"
+
+// account_id = 23541
+
+email_id = 'muthu@gmail.comc';
+password = 252525;
+City = "Delhi"
+
+console.log(email_id);
+
+console.table([account_id,email_id,password,City]);
